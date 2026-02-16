381d6f285a50ded97722d5f17745c761b2e97511
xenia <xenia@test.ru>
2026-02-16 10:53:41 +0300

    Задание 1. Inline-функция

--- src/main/kotlin/ru/otus/homework/homework/processList.kt
+++ src/main/kotlin/ru/otus/homework/homework/processList.kt
@@ -8,7 +8,7 @@ inline fun processList(list: List<Int>, action: (Int) -> Unit) {
 
 fun skipThreeAndPrint(list: List<Int>) {
     processList(list) {
-        if (it == 3) return
+        if (it == 3) return@processList
         println("Processing $it")
     }
 }
